acba6e67e150b92733026bba87bb598717de017f
DSarceno <dsarceno68@gmail.com>
2021-01-12 12:23:06 -0600

    pe1.c

--- cPrograms/pe1.c
+++ cPrograms/pe1.c
@@ -4,3 +4,14 @@
 // Problem 1 Project Euler
 //
 // Purpose: Sum of all the multiples of 3 or 5 below 1000
+#include <stdio.h>
+
+int main( void ) {
+  int sum, i;
+  for (i = 1; i <= 1000; i++){
+    if ((i % 3) && (i % 5)){
+      sum += i;
+    }
+  }
+  printf("%d \n", sum);
+}

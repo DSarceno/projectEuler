98d6921d1409b666d998b8e1e9aa6549e7646186
DSarceno <dsarceno68@gmail.com>
2021-01-12 14:16:02 -0600

    first 2 in c

--- cPrograms/pe1.c
+++ cPrograms/pe1.c
@@ -7,9 +7,10 @@
 #include <stdio.h>
 
 int main( void ) {
-  int sum, i;
-  for (i = 1; i <= 1000; i++){
-    if ((i % 3) && (i % 5)){
+  int sum;
+  sum = 0;
+  for (unsigned int i = 3; i < 1000; i++){
+    if ((i % 3 == 0) || (i % 5 == 0)){
       sum += i;
     }
   }
--- cPrograms/pe2.c
+++ cPrograms/pe2.c
@@ -4,3 +4,24 @@
 // Problem 1 Project Euler
 //
 // Purpose: Sum of all the even fibonacci numbers below 4000000
+
+#include <stdio.h>
+
+int main( void ){
+  int a, b, step, sum; // Variable declaration
+
+  // Values
+  a = 0;
+  b = 1;
+  sum = 0;
+
+  while (a <= 4000000){
+    if (a % 2 == 0){
+      sum += a;
+    }
+    step = a;
+    a = b;
+    b += step;
+  }
+  printf("%d \n", sum);
+}

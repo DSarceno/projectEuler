f7dffa7aaf836fb2368f200b8ac5ef246cb01ea1
DSarceno <dsarceno68@gmail.com>
2022-03-25 13:38:02 -0600

    Actualizacion

---
+++ pythonPrograms/largestProductInAGrid.py
@@ -0,0 +1,9 @@
+# @Author: Diego Sarceno
+# 21.01.2022
+
+# Problem 11 of Project Euler
+
+'''
+Tomando los datos del grid de 20x20 dado, se toman todos los grupos de 4 adyacentes (arriba, abajo, lados y diagonales) y encontrar el producto más grande.
+'''
+# tomamos el numero dado en el problema
